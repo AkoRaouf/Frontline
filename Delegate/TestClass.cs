042001230f14263739dcdbb767a4b814b6abb317
Raouf.Modaresi <Raouf.Modaresi@IT-MODARESI>
2020-05-31 10:39:02 +0430

    test class for delegate

--- Delegate/TestClass.cs
+++ Delegate/TestClass.cs
@@ -6,17 +6,36 @@ namespace Frontline.Delegate
 {
     public class TestClass
     {
-        public static int GetIndexOfFirtsNonEmptyBin(int[] Bins) => Array.FindIndex(Bins, IsGreaterThanZero);
+        public int GetIndexOfFirtsNonEmptyBin(int[] Bins) => Array.FindIndex(Bins, IsGreaterThanZero);
 
-        private static bool IsGreaterThanZero(int value) => value > 0;
+        public int GetIndexOfFirtsNonEmptyBinlambda(int[] Bins) => Array.FindIndex(Bins, value => value > 0);
 
-        public static void FindMyItem()
+        private bool IsGreaterThanZero(int value) => value > 0;
+
+        private Predicate<int> IsGreaterThanZeroP(int threshold)
+        {
+            return value => value > threshold;
+        }
+        private Predicate<int> IsGreaterThanZeroPp(int threshold) => value => value > 0;
+
+        public void FindMyItem()
         {
             var p = new Predicate<int>(IsGreaterThanZero);
             p.Invoke(10);
 
             Func<int, bool> func = IsGreaterThanZero;
             var res = func(1);
+
+            Predicate<string> isEq = s => s == "1";
+
+            DoMyPredicate(10, s => s == 1);
+        }
+
+        public void DoMyPredicate<T>(T aa, Predicate<T> predicate) { }
+
+        public void DoMyFunc<T>(Func<T, bool> func)
+        {
+
         }
     }
 }
