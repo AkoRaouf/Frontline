88017f241af3c646119e67eeafe46ad412d03ff0
Raouf.Modaresi <Raouf.Modaresi@IT-MODARESI>
2020-06-15 18:00:39 +0430

    set

---
+++ Delegate/Anonymous.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Frontline.Delegate
+{
+    public static class Anonymous
+    {
+        public static void Caught()
+        {
+            var greaterThanN = new Predicate<int>[10];
+            for (int i = 0; i < greaterThanN.Length; ++i)
+            {
+                var current = i;
+                greaterThanN[i] = value => value > current; // Bad use of i
+            }
+            var aa = (greaterThanN[5](20));
+            var cc = (greaterThanN[5](6));
+        }
+    }
+}
---
+++ Expression/Lambda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Frontline.Expression
+{
+    public class Lambda
+    {
+        public void TestIt()
+        {
+            Expression<Func<int, bool>> greaterThanN = value => value > 10;
+
+
+        }
+    }
+}
